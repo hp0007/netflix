ef1a251ab52d62680b258f87c34c578de296cbe5
hp0007 <harshitgajjar95336@gmail.com>
2021-03-07 15:06:43 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Home from './pages/Home'
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div >
-        <Home />
+    <div>
+      <Home />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,65 +1,80 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom'
-import Logo from '../assets/images/logo.png'
-import {BiSearch} from 'react-icons/bi'
-import {BsFillBellFill} from 'react-icons/bs'
-import account from '../assets/images/acc1.jpg'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../assets/images/logo.png";
+import { BiSearch } from "react-icons/bi";
+import { BsFillBellFill } from "react-icons/bs";
+import account from "../assets/images/acc1.jpg";
 
 const Header = () => {
-    const navData = [
-        {
-            id:1,
-            name:"Home",
-            path:"/netflix"
-        },
-        {
-            id:2,
-            name:"Tv Shows",
-            path:"/tvShows"
-        },
-        {
-            id:3,
-            name:"Movies",
-            path:"/movies"
-        },
-        {
-            id:4,
-            name:"Series",
-            path:"/series"
-        }
-    ]
-    return(
-        <div>
-            <div className="header">
-                <nav className="navbar navbar-expand-lg ">
-                    <img className="ml-3" src={Logo}/>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ml-auto mr-auto">
-                            {
-                                navData.map(data => {
-                                    return(
-                                        <li className="nav-item ml-5" key={data.id}>
-                                            <NavLink className="nav-link" to={data.path} exact activeClassName="active">{data.name}</NavLink>
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ul>
-                    </div>
-                    <div className="icons d-flex">
-                        <i><BiSearch /></i>
-                        <i className="ml-3"><BsFillBellFill /></i>
-                        <div className="account">
-                            <img src={account}/> 
-                        </div>
-                    </div>
-                </nav>
+  const navData = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/netflix",
+    },
+    {
+      id: 2,
+      name: "Tv Shows",
+      path: "/tvShows",
+    },
+    {
+      id: 3,
+      name: "Movies",
+      path: "/movies",
+    },
+    {
+      id: 4,
+      name: "Series",
+      path: "/series",
+    },
+  ];
+  return (
+    <div>
+      <div className="header">
+        <nav className="navbar navbar-expand-lg ">
+          <img className="ml-3" src={Logo} />
+          <button
+            class="navbar-toggler collapsed"
+            data-target="#navbarNav"
+            data-toggle="collapse"
+          >
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto mr-auto">
+              {navData.map((data) => {
+                return (
+                  <li className="nav-item ml-5" key={data.id}>
+                    <NavLink
+                      className="nav-link"
+                      to={data.path}
+                      exact
+                      activeClassName="active"
+                    >
+                      {data.name}
+                    </NavLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="icons d-flex">
+            <i>
+              <BiSearch />
+            </i>
+            <i className="ml-3">
+              <BsFillBellFill />
+            </i>
+            <div className="account">
+              <img src={account} />
             </div>
-        </div>
-    )
-}
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
